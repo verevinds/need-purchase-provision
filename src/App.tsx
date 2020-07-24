1ad111e55f8b4cbce787e01433a0dc32b7d435b2
verevinds <verevinds@yandex.ru>
2020-07-24 11:34:33 +0700

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
-import React, { memo, Fragment } from 'react';
-export interface IApp {}
+import * as React from 'react';
 
-const App: React.FC<IApp> = (props) => {
-  return <Fragment></Fragment>;
+const App = () => {
+  return (
+    <div id='body'>
+      <div id='header'>
+        <a id='logo' href='http://www.c31.nccp.ru/'>
+          <img src='//api.nccp-eng.ru/images/logo2.png' />
+        </a>
+
+        <div id='menu'>
+          <div className='point' data-id='5'>
+            Главная
+          </div>
+          <div className='point' data-id='1'>
+            Потребности
+          </div>
+          <div className='point' data-id='2'>
+            Позиции
+          </div>
+          <div className='point' data-id='3'>
+            Заявки
+          </div>
+          <div className='point' data-id='4'>
+            Наименования
+          </div>
+          <div className='point' data-id='6'>
+            Склад
+          </div>
+        </div>
+        <div id='user'></div>
+      </div>
+
+      <div id='content'></div>
+    </div>
+  );
 };
 
-export default memo(App);
+export default React.memo(App);
---
+++ webpack.config.js
@@ -0,0 +1,157 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+  if (isProd) {
+    config.minimizer = [new OptimizeCssAssetWebpackPlugin(), new TerserWebpackPlugin()];
+  }
+  return config;
+};
+
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
+
+const cssLoaders = (extra) => {
+  const loader = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true,
+      },
+    },
+    'css-loader',
+  ];
+
+  if (extra) {
+    loader.push(extra);
+  }
+
+  return loader;
+};
+
+const babelOptions = (preset) => {
+  const opts = {
+    presets: ['@babel/preset-env'],
+    plugins: ['@babel/plugin-proposal-class-properties'],
+  };
+
+  if (preset) {
+    opts.presets.push(preset);
+  }
+
+  return opts;
+};
+
+const pluginsBase = () => {
+  const base = [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist'),
+      },
+    ]),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+  ];
+
+  return base;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'production',
+  entry: {
+    main: ['@babel/polyfill', './index.tsx'],
+  },
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    hot: isDev,
+  },
+  devtool: isDev ? 'source-map' : '',
+  plugins: pluginsBase(),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: cssLoaders(),
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: cssLoaders('less-loader'),
+      },
+      {
+        test: /\.s[ac]ss$/,
+        exclude: /node_modules/,
+        use: cssLoaders('sass-loader'),
+      },
+      {
+        test: /\.(ttf|woff|woff2)$/,
+        exclude: /node_modules/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: babelOptions(),
+        },
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: babelOptions('@babel/preset-typescript'),
+        },
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: babelOptions('@babel/preset-react'),
+        },
+      },
+      {
+        test: /\.tsx$/,
+        exclude: /node_modules/,
+
+        loader: 'awesome-typescript-loader',
+      },
+    ],
+  },
+};
