4e69d78f3a50e0b5eb93bf1414afa502ff7cc95b
verevinds <verevinds@yandex.ru>
2020-07-24 14:35:16 +0700

    Implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header/Header';
+import ClaimsPage from './pages/ClaimsPage';
 import MainPage from './pages/MainPage';
+import NeedsPage from './pages/NeedsPage';
+import NominationPage from './pages/NominationPage';
+import PositionsPage from './pages/PositionsPage';
+import StockPage from './pages/StockPage';
 
 const App = () => {
   return (
@@ -10,8 +15,15 @@ const App = () => {
       <Header />
 
       <Switch>
+        <Route exact path='/needs' component={NeedsPage} />
+        <Route exact path='/positions' component={PositionsPage} />
+        <Route exact path='/claims' component={ClaimsPage} />
+        <Route exact path='/nominations' component={NominationPage} />
+        <Route exact path='/stock' component={StockPage} />
+
         <Route exact path='/' component={MainPage} />
-        <Route path='/needs/:id' component={MainPage} />
+        <Route path='/main' component={MainPage} />
+        <Route path='/main/:id' component={MainPage} />
       </Switch>
     </BrowserRouter>
   );
