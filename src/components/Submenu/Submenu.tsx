6477fcd1b862240f7494ecd5881243f2826ae6d6
verevinds <verevinds@yandex.ru>
2020-07-27 14:38:30 +0700

    Add submenu components

---
+++ src/components/Submenu/Submenu.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+import { TLink } from '../HeaderMenu/HeaderMenu';
+import './Submenu.scss';
+
+export interface ISubmenu {
+  linksList: TLink[];
+}
+
+const Submenu: React.FC<ISubmenu> = (props) => {
+  return (
+    <div className='submenu'>
+      {props.linksList.map((_link: TLink, _index: number) => (
+        <NavLink
+          exact
+          to={`${_link?.url === '/' ? '' : _link?.url || ''}/${_link.link}`}
+          className='submenu__point'
+          activeClassName='submenu__point_active'
+          data-id={`${_index}`}
+          key={_index}
+        >
+          {_link.name}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+export default React.memo(Submenu);
