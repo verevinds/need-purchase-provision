45c28a266315e9ad18c17232355499a49a742272
verevinds <verevinds@yandex.ru>
2020-07-27 14:39:59 +0700

    Add subscriptions page

---
+++ src/layout/MainPage/SubscriptionToOrders.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export interface ISubscriptionToOrders {}
+
+const SubscriptionToOrders: React.FC<ISubscriptionToOrders> = () => {
+  return <>subscription-to-orders</>;
+};
+
+export default React.memo(SubscriptionToOrders);
