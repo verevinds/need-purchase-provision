353d35c928b6db5a04700cb98fb11b62576facb4
verevinds <verevinds@yandex.ru>
2020-07-27 09:02:14 +0700

    Add user logo & name in header

--- src/components/HeaderUser/HeaderUser.tsx
+++ src/components/HeaderUser/HeaderUser.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
+import { useSelector } from 'react-redux';
+import { IState } from 'src/redux/reducer';
+import { TUser } from 'src/redux/reducer/usersReducer';
 
 export interface IHeaderUser {}
 
 const HeaderUser: React.FC<IHeaderUser> = () => {
-  return <div id='user' className='header__user'></div>;
+  const user: TUser = useSelector((state: IState) => state.auth.user);
+
+  return (
+    <div id='user' className='header__user'>
+      <div
+        className='header__user__img'
+        style={{
+          backgroundImage: `url('${user?.photo}')`,
+        }}
+      ></div>
+      <div className='header__user__name'>Веревин Денис Сергеевич</div>
+    </div>
+  );
 };
 
 export default React.memo(HeaderUser);
