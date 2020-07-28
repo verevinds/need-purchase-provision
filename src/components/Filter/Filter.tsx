311999fb37e9b21378d227f000bc7a8d9922ae35
verevinds <verevinds@yandex.ru>
2020-07-28 15:46:59 +0700

    Clear code

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -2,14 +2,11 @@ import * as React from 'react';
 import Submenu from '../Submenu/Submenu';
 import './Filter.scss';
 
-export interface IFilter {}
-
-const Filter: React.FC<IFilter> = () => {
+const Filter: React.FC = () => {
   const linksList = [
     { name: 'Статистика', url: '/main', link: 'statistics' },
     { name: 'Подписка на заказы', url: '/main', link: 'subscription-to-orders' },
     { name: 'О системе', url: '/main', link: 'about' },
-    { name: 'СУОД', url: '/main', link: 'suod' },
     { name: 'Руководство оператора', url: '/main', link: 'operator-manual' },
   ];
   return (
--- src/components/Submenu/Submenu.tsx
+++ src/components/Submenu/Submenu.tsx
@@ -7,18 +7,17 @@ export interface ISubmenu {
   linksList: TLink[];
 }
 
-const Submenu: React.FC<ISubmenu> = (props) => {
+const Submenu: React.FC<ISubmenu> = ({ linksList }) => {
   return (
     <div className='submenu'>
-      {props.linksList.map((_link: TLink, _index: number) => (
+      {linksList.map((_link: TLink, _index: number) => (
         <NavLink
           exact
           to={`${_link?.url === '/' ? '' : _link?.url || ''}/${_link.link}`}
           className='submenu__point'
           activeClassName='submenu__point_active'
           data-id={`${_index}`}
-          key={_index}
-        >
+          key={_link.link}>
           {_link.name}
         </NavLink>
       ))}
