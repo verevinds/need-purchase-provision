39bd6bd9959cc0f651e98426ae946e8b7fd1eb03
verevinds <verevinds@yandex.ru>
2020-08-07 09:37:39 +0700

    Refactoring

--- src/components/Switch/__test__/Switch.test.js
+++ src/components/Switch/__test__/Switch.test.js
@@ -1,31 +1,56 @@
 import React from 'react';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer, { act } from 'react-test-renderer';
+import toJson from 'enzyme-to-json';
 
 import Switch from '../Switch';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-// props { size = 10, text, checked, id, onClick }
 describe('<Switch />', () => {
-  const component = renderer.create(<Switch id={1} text='Мои заявки' />);
-  const instance = component.root;
-  const input = instance.findByType('input');
-  const span = instance.findByType('span');
+  const mockFn = jest.fn();
+  const initialProps = { size: 15, text: 'Мои заявки', id: 1, onClick: mockFn };
+  const jsxComponent = (
+    <Switch
+      id={initialProps.id}
+      text={initialProps.text}
+      onClick={initialProps.onClick}
+      size={initialProps.size}
+    />
+  );
 
-  test('render success', () => {
-    expect(component.toJSON()).toMatchSnapshot();
+  const wrapper = shallow(jsxComponent);
+  const component = mount(jsxComponent);
+  const input = wrapper.find('.switch__input');
+  const span = wrapper.find('.switch__text');
+
+  it('should be defined', () => {
+    expect(Switch).toBeDefined();
   });
-  test('required props "id" not to be undefined', () => {
-    expect(input.props.id).not.toBeUndefined();
+  it('render success', () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
-  test('it text component is visible', () => {
-    expect(span.props.children).toEqual('Мои заявки');
+  it('should have a input id', () => {
+    expect(typeof component.props().id).toBe('number');
+    expect(component.props().id).not.toBeUndefined();
+    expect(input.props().id).toEqual(initialProps.id);
+    expect(input.props().type).toEqual('checkbox');
   });
-  test('checkbox changes his value on the click', () => {
+  it('it text component is visible', () => {
+    expect(span.props().children).toEqual(initialProps.text);
+  });
+  it('checkbox changes his value on the click', () => {
+    const component = renderer.create(jsxComponent);
+    const instance = component.root;
+    const input = instance.findByType('input');
+
     expect(input.props.checked).toEqual(false);
-    act(() => input.props.onClick());
+    act(() => input.props.onChange());
     expect(input.props.checked).toEqual(true);
+  });
+  it('should have a click function', () => {
+    wrapper.find('input[type="checkbox"]').simulate('change');
+    expect(mockFn).toHaveBeenCalled();
   });
 });
