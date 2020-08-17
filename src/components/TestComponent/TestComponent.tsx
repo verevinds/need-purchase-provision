fb6e7d807e02eb8f94e229ab647397682d221d03
Веревин Денис <51701139+verevinds@users.noreply.github.com>
2020-08-17 15:17:11 +0700

    Add import user from store

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
+import { useSelector } from 'react-redux';
+import { IState } from '../../redux/reducer';
 
 export interface ITestComponent {}
 
 const TestComponent: React.FC<ITestComponent> = () => {
+  const { user } = useSelector((state: IState) => state.auth);
+
+  React.useEffect(() => {
+    console.log(user);
+  }, [user]);
+
   return (
     <>
       <h1>Hello</h1>
