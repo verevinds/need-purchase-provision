2c8f52b07e0489eefa98ad748437c6a804550b39
Подвойский Кирилл Павлович <seunsteam@mail.ru>
2020-08-17 14:54:10 +0700

    Add edit

--- src/pages/ClaimsPage.tsx
+++ src/pages/ClaimsPage.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export interface IClaimsPage {}
 
 const ClaimsPage: React.FC<IClaimsPage> = () => {
-  return <>Claims</>;
+  return <>Claims edit</>;
 };
 
 export default React.memo(ClaimsPage);
